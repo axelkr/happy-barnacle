4c8c5b0ddd9babad3867381e8ddc2eb5eff5e049
Axel <axel@sweetas.com>
2021-01-17 08:41:49 +0100

    provide endpoints

--- src/Server.ts
+++ src/Server.ts
@@ -2,6 +2,7 @@ import { Logger } from 'sitka';
 import express from 'express';
 
 import {Database} from './Database';
+import {ObjectEvent} from './objectEvent';
 
 export class Server {
     private logger: Logger;
@@ -16,8 +17,32 @@ export class Server {
         const app = express();
         const PORT = 8000;
         app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+        app.get('/objectEvent', (req,res) => {
+            if (!req.query.hasOwnProperty('topic')) {
+                res.status(400).send('parameter topic missing');
+                return;
+            }
+            const objectEvents = this.db.query(req.query.topic as string);
+            res.status(200).send(objectEvents);
+        })
+        app.post('objectEvent', (req,res) => {
+            this.logger.debug("received: ", req.query);
+            // TODO: validate input, extract payload
+            const inputObjectEvent : ObjectEvent = {
+                topic: req.query.topic as string,
+                time: new Date(),
+                id: parseInt(req.query.id as string),
+                eventType: req.query.eventType as string,
+                object: req.query.object as string,
+                objectType: req.query.objectType as string,
+                payload: new Map<string,string>()
+            }
+            const objectEvent = this.db.store(inputObjectEvent);
+            res.status(200).send(objectEvent);
+        })
         app.listen(PORT, () => {
-            this.logger.debug(`Server is running at https://localhost:${PORT}`);
+            this.logger.debug(`Server is running at http://localhost:${PORT}`);
         });
-	}
+    }
+
 }
