b80dff0bd0193f9b8d0799bb9cc1bf02f032a462
axelkr <axelkr.dev@gmail.com>
2021-01-21 09:31:16 +0100

    further improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.1.4
+- reduce package size further
+- package can be loaded by node packages
+
 # v1.1.3
 - provide .d.ts files so package can be referenced from Typescript
 - reduced package's content to bare minimum
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'happyBarnacle',
+      globals :{
+          sitka:'sitka', 
+          express: 'express', 
+          cors: 'cors',
+          'better-sqlite3': 'sqlite'
+      }
+    },
+    plugins: [
+        typescript({
+          typescript: require('typescript'),
+        }),
+    ],
+    external: [ 'sitka','cors','express','better-sqlite3' ]
+  };
